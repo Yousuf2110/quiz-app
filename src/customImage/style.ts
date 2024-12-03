ee9843326ce5967e9a9edc8c06faf85e1e84efad
mzh175667 <mzh175667@gmail.com>
2024-12-03 12:38:50 +0500

    ui fixes

---
+++ src/customImage/style.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import {RFPercentage} from 'react-native-responsive-fontsize';
+
+export const styles = StyleSheet.create({
+  image: {
+    width: '60%',
+    height: '60%',
+    borderWidth: 2,
+  },
+});
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {styles} from './styles';
 import {SCREEN} from '../../constants/screen';
 import {useNavigation} from '@react-navigation/native';
+import MainButton from '../../assets/button';
+import CustomImage from '../../customImage';
+import Button from '../../assets/button';
 
 const Home = () => {
   const navigation: any = useNavigation();
+  const [quizLevel, setQuizLevel] = useState('');
+
+  // Function to handle button press and set quiz level
+  const handlePress = (level: string) => {
+    setQuizLevel(level);
+    navigation.navigate(SCREEN.GAME, {level}); // Pass level to the game screen
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text style={styles.title}>Home</Text>
-        <Button
-          title="Press"
-          onPress={() => navigation.navigate(SCREEN.GAME)}
+      <View>
+        <Image
+          source={require('../../assets/images/mind.png')}
+          style={{width: 200, height: 200}}
         />
       </View>
+      <View style={styles.innerContainer}>
+        <Button title={'Easy'} onPress={() => handlePress('easy')} />
+        <Button title={'Normal'} onPress={() => handlePress('normal')} />
+        <Button title={'Hard'} onPress={() => handlePress('hard')} />
+      </View>
+
+      {/* Display the current quiz level for testing */}
+      {quizLevel && <Text>Selected Level: {quizLevel}</Text>}
     </View>
   );
 };
